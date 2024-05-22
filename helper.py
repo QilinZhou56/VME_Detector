be148e3287145ae3fff2251a96fe187c6624fe6a
Qilin Zhou <148006921+QilinZhou56@users.noreply.github.com>
2024-05-22 17:14:49 -0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -48,7 +48,7 @@ def load_vit_model(model_path):
     model.eval()
     return model
 
-@st.cache_data(ttl=24, hash_funcs={"MyUnhashableClass": lambda _: None})
+@st.cache_data(ttl=24)
 def load_owlvit_model():
     processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
     model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32")
